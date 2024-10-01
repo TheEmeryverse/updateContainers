ea131ad1cc1b74590a10c2bf8b50a07f00e9502b
TheEmeryverse <server@emeryverse.com>
2024-09-30 22:33:29 -0500

    shut down, rm, and update and run cmds for sonarr and radarr

--- updateContainers.sh
+++ updateContainers.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 #stop all containers
-#echo "Stopping all containers..."
+echo "Stopping all containers..."
 #docker stop prowlarr; docker stop overseerr; docker stop tautulli; sudo docker stop qbittorrent; sudo docker stop bazarr; sudo stop readarr; sudo docker stop plex; sudo docker stop sonarr; sudo docker stop radarr
+sudo docker stop sonarr; sudo docker stop radarr
 
 #uodate all containers
 echo "Updating all containers..."
@@ -16,3 +17,39 @@ sudo docker pull lscr.io/linuxserver/plex:latest
 sudo docker pull hotio/sonarr:latest
 sudo docker pull hotio/radarr:latest
 
+echo "Deleting all containers"
+#docker rm prowlarr; docker rm overseerr; docker rm tautulli; sudo docker rm qbittorrent; sudo docker rm bazarr; sudo docker rm readarr; sudo docker plex; sudo docker rm sonarr; sudo docker rm radarr
+sudo docker rm radarr; sudo docker rm sonarr
+
+
+echo "All containers successfully deleted..."
+
+echo "Starting containers with updated images..."
+
+echo "Starting radarr..."
+sudo docker run -d \
+	--name=radarr \
+	--hostname=2d9eea69def0 \
+	--mac-address=02:42:ac:11:00:06 \
+	--volume=/home/hudson/radarr/config:/config \
+	--volume=/home/hudson/data/torrents:/downloads \
+	--volume=/home/hudson/data/media/movies:/movies \
+	--network=bridge \
+	-p 7878:7878 \
+	--restart=unless-stopped \
+	--runtime=runc \
+hotio/radarr:latest
+
+echo "Starting sonarr..."
+sudo docker run -d \
+	--name=sonarr \
+	--hostname=c8a71a008167 \
+	--mac-address=02:42:ac:11:00:05 \
+	--volume=/home/hudson/sonarr/config:/config \
+	--volume=/home/hudson/data/torrents/:/downloads \
+	--volume=/home/hudson/data/media/tv:/tv \
+	--network=bridge \
+	-p 8989:8989 \
+	--restart=unless-stopped \
+	--runtime=runc \
+hotio/sonarr:latest
