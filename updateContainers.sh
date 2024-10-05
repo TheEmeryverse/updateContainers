2b604d88ddd471ff6f7d55723b4e8ba7202d3081
The Emeryverse <server@emeryverse.com>
2024-10-05 02:13:15 -0500

    Update updateContainers.sh

--- updateContainers.sh
+++ updateContainers.sh
@@ -57,6 +57,7 @@ tautulli/tautulli:latest
 echo "Starting qbittorrent..."
 docker run -d \
 	--name qbittorrent \
+	--network=host \
 	-e VPN_TYPE=wireguard \
 	-e WEBUI_PASSWORD=@elLazo4932@ \
 	-e TZ=America/Chicago \
