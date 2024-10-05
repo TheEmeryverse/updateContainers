5e6b315d139895f386da865d50fcb703871ef1d0
TheEmeryverse <server@emeryverse.com>
2024-10-05 12:13:05 -0500

    Update qbittorrentTest.sh

--- qbittorrentTest.sh
+++ qbittorrentTest.sh
@@ -28,9 +28,6 @@ docker run \
 	-e WEBUI_PORT=8090 \
 	-v /home/containerConfigs/qbittorrentTest/config:/config \
 	-v /mnt/plexNAS/torrents:/downloads \
-	# -p 8090:8090 \
-	# -p 9050:9050/udp \
-	# -p 9050:9050 \
 	--restart=unless-stopped \
 	lscr.io/linuxserver/qbittorrent
 
